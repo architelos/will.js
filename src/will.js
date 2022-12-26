a5cdaeb642cbd3cc0431ae1b394ffcf9e21dba93
architelos <technopiabcde@gmail.com>
2022-12-26 09:52:47 +0700

    feat: add more methods

--- src/will.js
+++ src/will.js
@@ -48,6 +48,33 @@ Object.defineProperty(Will.prototype, "not", {
 });
 
 /**
+ * Asserts that an object is null.
+ * 
+ * @api public
+ */
+Will.prototype.null = function () {
+    this.assert(this.actual === null, "null", null);
+};
+
+/**
+ * Asserts that an object is undefined.
+ * 
+ * @api public
+ */
+Will.prototype.undefined = function () {
+    this.assert(this.actual === undefined, "undefined", "undefined");
+};
+
+/**
+ * Asserts that an object is a boolean.
+ * 
+ * @api public
+ */
+Will.prototype.boolean = function () {
+    this.assert(typeof this.actual === "a boolean");
+};
+
+/**
  * Asserts that an object is true.
  * 
  * @api public
@@ -63,6 +90,87 @@ Will.prototype.true = function () {
  */
 Will.prototype.false = function () {
     this.assert(this.actual === false, "false", false);
+};
+
+/**
+ * Asserts that an object is a number.
+ * 
+ * @api public
+ */
+Will.prototype.number = function () {
+    this.assert(typeof this.actual === "number", "a number");
+};
+
+/**
+ * Asserts that an object is a bigint.
+ * 
+ * @api public
+ */
+Will.prototype.bigint = function () {
+    this.assert(typeof this.actual === "bigint", "a bigint");
+};
+
+/**
+ * Asserts that an object is NaN.
+ * 
+ * @api public
+ */
+Will.prototype.nan = function () {
+    this.assert(isNan(this.actual), "NaN");
+};
+
+/**
+ * Asserts that an object is a string.
+ * 
+ * @api public
+ */
+Will.prototype.string = function () {
+    this.assert(typeof this.actual === "string", "a string");
+};
+
+/**
+ * Asserts that an object is a symbol.
+ * 
+ * @api public
+ */
+Will.prototype.symbol = function () {
+    this.assert(typeof this.actual === "symbol", "a symbol");
+};
+
+/**
+ * Asserts that an object is an object.
+ * 
+ * @api public
+ */
+Will.prototype.object = function () {
+    let isObject = false;
+
+    if (typeof this.actual === "object"
+        && !Array.isArray(this.actual)
+        && this.actual !== null
+        && typeof this.actual !== "function") {
+        isObject = true;
+    }
+
+    this.assert(isObject, "an object");
+};
+
+/**
+ * Asserts that an object is a function.
+ * 
+ * @api public
+ */
+Will.prototype.function = function () {
+    this.assert(typeof this.actual === "function", "a function");
+};
+
+/**
+ * Asserts that an object is an array.
+ * 
+ * @api public
+ */
+Will.prototype.array = function () {
+    this.assert(Array.isArray(this.actual), "an array");
 };
 
 /**
@@ -93,7 +201,7 @@ Will.prototype.falsy = function () {
  */
 Will.prototype.empty = function () {
     let isEmpty = false;
-    
+
     if (!this.actual) {
         isEmpty = true;
     } else if (typeof this.actual === "string" || Array.isArray(this.actual)) {
@@ -103,6 +211,72 @@ Will.prototype.empty = function () {
     }
 
     this.assert(isEmpty, "empty");
+};
+
+/**
+ * Asserts that an object is greater than another object.
+ *  
+ * @param {number} expected
+ * @api public
+ */
+Will.prototype.greaterThan = function (expected) {
+    this.assert(this.actual > expected, `greater than ${expected}`);
+};
+
+/**
+ * Asserts that an object is greater than or equals another object.
+ * 
+ * @param {number} expected
+ * @api public
+ */
+Will.prototype.greaterThanOrEqual = function (expected) {
+    this.assert(this.actual >= expected, `greater than or equal to ${expected}`);
+};
+
+/**
+ * Asserts that an object is greater than or equals another object.
+ * 
+ * @param {number} expected
+ * @api public
+ */
+Will.prototype.greaterThanOrEqualTo = Will.prototype.greaterThanOrEqual;
+
+/**
+ * Asserts that an object is less than another object.
+ * 
+ * @param {number} expected
+ * @api public 
+ */
+Will.prototype.lessThan = function (expected) {
+    this.assert(this.actual < expected, `less than ${expected}`);
+};
+
+/**
+ * Asserts that an object is less than or equals another object.
+ * 
+ * @param {number} expected
+ * @api public
+ */
+Will.prototype.lessThanOrEqual = function (expected) {
+    this.assert(this.actual <= expected, `less than or equal to ${expected}`);
+};
+
+/**
+ * Asserts that an object is less than or equals another object.
+ * 
+ * @param {number} expected
+ * @api public
+ */
+Will.prototype.lessThanOrEqualTo = Will.prototype.lessThanOrEqual;
+
+/**
+ * Asserts that an object is an instance of a class.
+ * 
+ * @param aClass
+ * @api public 
+ */
+Will.prototype.instanceOf = function (expected) {
+    this.assert(this.actual instanceof expected, `an instance of ${expected.displayName || expected.name || inspect(expected)}`);
 };
 
 /**
